3d352ae8f9bc1c5d7bc1ca7422ac8e9aee0ccb77
Vinicius Zhu <vinicius.zhu@gmail.com>
2019-03-14 22:19:08 -0300

    Adicionando foreground dispatcher

--- app/src/main/java/br/edu/ifsp/scl/nfctagsdmkt/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/nfctagsdmkt/MainActivity.kt
@@ -1,12 +1,21 @@
 package br.edu.ifsp.scl.nfctagsdmkt
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.nfc.NfcAdapter.*
+import android.nfc.tech.MifareClassic
+import android.nfc.tech.Ndef
+import android.nfc.tech.NdefFormatable
+import android.nfc.tech.NfcA
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     val nfcWorker = NfcWorker()
+    lateinit var adaptadorNfc: NfcAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -17,6 +26,47 @@ class MainActivity : AppCompatActivity() {
                 escritaEt.isEnabled = false
             }
         }
+
+        adaptadorNfc = NfcAdapter.getDefaultAdapter(this)
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        val intent = Intent(this, javaClass).apply {
+            addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP)
+        }
+
+        val pendingIntent = PendingIntent.getActivity(this,0, intent, 0)
+
+        val ndefIf = IntentFilter(ACTION_NDEF_DISCOVERED, "text/plain")
+        val techIf = IntentFilter(ACTION_TECH_DISCOVERED)
+        val tagIf = IntentFilter(ACTION_TAG_DISCOVERED)
+        val ifArray = arrayOf(ndefIf ,techIf,tagIf)
+
+        val listaTecsArray = arrayOf(
+            arrayOf<String>(
+                NfcA::class.java.name,
+                Ndef::class.java.name,
+                MifareClassic::class.java.name
+            ),
+            arrayOf<String>(
+                NfcA::class.java.name,
+                NdefFormatable::class.java.name,
+                MifareClassic::class.java.name
+            )
+        )
+
+        adaptadorNfc.enableForegroundDispatch(
+            this,
+            pendingIntent,
+            ifArray,
+            listaTecsArray)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        adaptadorNfc.disableForegroundDispatch(this)
     }
 
     override fun onNewIntent(intent: Intent) {
